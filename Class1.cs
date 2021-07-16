eb08cdddbe7b04e620e7f3f42c5637e27a62b043
DarkMessiah5 <pavekum@mail.ru>
2021-07-16 15:40:45 +0600

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace StudentAspirantClasses
+{
+    class Student
+    {
+        private string _firstName, _lastName, _group;
+        public readonly double _averageMark;
+
+        const double STUDENT_SCOLARSHIP_COEFF = 1.25;
+
+        public Student()
+        {
+            Console.Write("Введите имя студента: ");
+            _firstName = Console.ReadLine();
+
+            Console.Write("Введите фамилию студента: ");
+            _lastName = Console.ReadLine();
+
+            Console.Write("Введите группу студента: ");
+            _group = Console.ReadLine();
+
+            Console.Write("Введите среднюю оценку студента: ");
+            _averageMark = System.Convert.ToDouble(Console.ReadLine());
+        }
+        public Student(string firstName, string lastName, string group, double averageMark)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+            _group = group;
+            _averageMark = averageMark;
+        }
+
+        public virtual int GetScolarship()
+        {
+            int x = 80;
+
+            if (_averageMark == 5.0)
+                return (int)(STUDENT_SCOLARSHIP_COEFF * x);
+            else if (_averageMark >= 4.0)
+                return x;
+            else
+                return 0;
+        }
+
+        public virtual void GetInfo()
+        {
+            Console.WriteLine("\nИмя: " + _firstName +
+                              "\nФамилия: " + _lastName +
+                              "\nГруппа: " + _group +
+                              "\nСредняя оценка: " + _averageMark);
+        }
+    }
+
+    class Aspirant : Student
+    {
+        private string _scientificWork;
+
+        const double ASPIRANT_SCOLARSHIP_COEFF = 2.25;
+
+        public Aspirant() : base()
+        {
+            Console.Write("Введите название научной работы: ");
+            _scientificWork = Console.ReadLine();
+        }
+        public Aspirant(string firstName, string lastName, string group, double averageMark, string scientificWork)
+               : base(firstName, lastName, group, averageMark)
+        {
+            _scientificWork = scientificWork;
+        }
+
+        public override int GetScolarship()
+        {
+            return (int)(base.GetScolarship() * ASPIRANT_SCOLARSHIP_COEFF);
+        }
+
+        public override void GetInfo()
+        {
+            base.GetInfo();
+            Console.WriteLine("Научная работа: " + _scientificWork);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+ Создайте пример наследования, реализуйте класс Student и класс Aspirant, аспирант отличается от студента наличием некой научной работы.
+    а) Класс Student содержит переменные: String firstName, lastName, group. А также, double averageMark, содержащую среднюю оценку.
+    б) Создать переменную типа Student, которая ссылается на объект типа Aspirant.
+    в) Создать метод getScholarship() для класса Student, который возвращает сумму стипендии. Если средняя оценка студента равна 5, 
+       то сумма 100 грн, иначе 80. Переопределить этот метод в классе Aspirant.  Если средняя оценка аспиранта равна 5, то сумма 200 грн, иначе 180.
+    г) Создать массив типа Student, содержащий объекты класса Student и Aspirant. Вызвать метод getScholarship() для каждого элемента массива.
+ */
+
+using System;
+
+namespace StudentAspirantClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new();
+
+            Student student2 = new("Pavel", "Kuminov", "430-4", 4.8);
+
+            Student student3 = new Aspirant();
+
+            Student student4 = new Aspirant("Pan", "Huilovski", "129-3", 3.5, "About something");
+
+            student1.GetInfo();
+            
+            student2.GetInfo();
+
+            student3.GetInfo();
+            
+            student4.GetInfo();
+        }
+    }
+}
